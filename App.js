2f679c76b36ae19f7fe52df37f99c2d956f1c112
Roland <goreczky.roland@gmail.com>
2024-08-29 16:08:14 +0200

    second bulild and still woring

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as ExpoExternalPurchase from 'expo-external-purchase';
+import { useEffect } from 'react';
 
 export default function App() {
+  useEffect(()=>{
+    if (ExpoExternalPurchase && ExpoExternalPurchase.version) {
+      console.log(`ExpoExternalPurchase modul version: ${ExpoExternalPurchase.version}`);
+      console.log(ExpoExternalPurchase);
+    } else {
+      console.log('ExpoExternalPurchase version information not exist');
+    }
+  },[])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>New content new build</Text>
       <StatusBar style="auto" />
       <Text>{ExpoExternalPurchase.hello()}</Text>
     </View>
