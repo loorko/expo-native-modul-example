3953b51529e1d0d9be7780112a43ef416f6acd18
Roland <goreczky.roland@gmail.com>
2024-09-03 17:10:06 +0200

    entitlements kiegészítés

--- App.js
+++ App.js
@@ -11,26 +11,32 @@ import { useEffect } from 'react';
 export default function App() {
   useEffect(()=>{
     console.log(ExpoExternalPurchase);
+    const subscription = ExpoExternalPurchase.addExternalPurchaseListener(({ result: newResult }) => {
+      console.log(newResult)
+    });
+    return () => subscription.remove();
   },[])
 
-  const checkIfCanPresent= async() => {
+  const checkIfCanPresent = async () => {
     try {
       const canPresent = await ExpoExternalPurchase.canPresentAsync();
       if (canPresent) {
-        Alert.alert('The notice sheet can be presented.')
         try {
-          const result = await ExpoExternalPurchase.presentNoticeSheetAsync('yolo');
+          const result = await ExpoExternalPurchase.presentNoticeSheetAsync();
           console.log('Notice sheet presented successfully:', result);
+          Alert.alert('It is working');
         } catch (error) {
           console.error('Failed to present notice sheet:', error);
+          Alert.alert('Failed to present notice sheet:', error.message); // Add error message for user feedback
         }
       } else {
-        Alert.alert('The notice sheet cannot be presented.')
+        Alert.alert('The notice sheet cannot be presented.');
       }
     } catch (error) {
       console.error('Failed to check if can present:', error);
+      Alert.alert('Failed to check if can present:', error.message); // Add error message for user feedback
     }
-  }
+  };
     
   return (
     <View style={styles.container}>
